92036528fcdea524c126cd2be1067e6dc9c67fa4
Junior Vidal <133786096+juniojtv@users.noreply.github.com>
2025-07-07 02:19:38 -0300

    Update challenge_01_defining_triangle_types.sql

--- challenge_01_defining_triangle_types.sql
+++ challenge_01_defining_triangle_types.sql
@@ -1,13 +1,13 @@
 /*
 https://www.hackerrank.com/challenges/what-type-of-triangle/problem
 Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
-
 Equilateral: It's a triangle with  sides of equal length.
 Isosceles: It's a triangle with  sides of equal length.
 Scalene: It's a triangle with  sides of differing lengths.
 Not A Triangle: The given values of A, B, and C don't form a triangle.
 */
---Training CASE logic
+-- SGBD or DBMS (Database Management System): DB2
+--Training CASE logic 
 SELECT
     CASE 
         WHEN NOT (    -- If the sum of 2 sides length is not larger than the other side, you can't form a triangle
